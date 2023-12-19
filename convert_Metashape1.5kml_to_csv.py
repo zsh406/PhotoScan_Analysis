8ba4a5675ef25999fab3210df0a2398a02c5621a
Shuhang Zhang <zhangsh52@mail.sysu.edu.cn>
2023-12-19 11:09:36 +0800

    Update convert_Metashape1.5kml_to_csv.py
    support Metashape 1.5.2

--- convert_Metashape1.5kml_to_csv.py
+++ convert_Metashape1.5kml_to_csv.py
@@ -2,9 +2,36 @@ import csv
 import xml.etree.ElementTree as ET
 
 SPEED = 8; # m/s, drone speed between waypoints
-kml_file = "Tianqin_MS-v2.kml"
+kml_file = r"E:\OneDrive - 中山大学\项目\2018_Stuttgart\Field test Tianqin 202312\规划\Tianqin_MS-v2-2.0_export-1219.kml"
 output_csv = 'output_MS_DJI.csv'
 output_csv_UGCS = 'output_MS_UGCS.csv'
+
+def extract_waypoint_info_to_UGCS_csv_v2(kml_file, output_csv):
+    tree = ET.parse(kml_file)
+    root = tree.getroot()
+
+    placemarks = root.findall('.//{http://www.opengis.net/kml/2.2}Placemark')
+
+    with open(output_csv, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        # writer.writerow(['Waypoint', 'Longitude', 'Latitude', 'Altitude', 'Heading', 'GimbalPitch', 'Actions'])
+
+        for placemark in placemarks:
+            name = placemark.find('.//{http://www.opengis.net/kml/2.2}name').text
+            if name == 'Wayline':
+                continue
+
+            coordinates = placemark.find('.//{http://www.opengis.net/kml/2.2}coordinates').text
+            
+            heading = placemark.findtext(".//{www.dji.com}heading")
+            gimbal_pitch = placemark.findtext(".//{www.dji.com}gimbalPitch")
+            actions = placemark.findtext(".//{www.dji.com}actions")
+
+            # Extract latitude, longitude, and altitude from coordinates
+            longitude, latitude, altitude = coordinates.split(',')
+
+            # writer.writerow([name, longitude, latitude, altitude, heading, gimbal_pitch, actions])
+            writer.writerow([latitude,longitude, altitude, '0',gimbal_pitch, heading])
 
 def extract_waypoint_to_DJI_csv(kml_file, output_csv):
     tree = ET.parse(kml_file)
@@ -28,7 +55,10 @@ def extract_waypoint_to_DJI_csv(kml_file, output_csv):
             range_value = lookat.find('.//{http://www.opengis.net/kml/2.2}range').text
             # altitude_mode = lookat.find('.//{http://www.opengis.net/kml/2.2}altitudeMode').text
             tilt = -tilt # DJI uses opposite sign for tilt
-
+            heading = int(float(heading));
+            if heading > 180:
+                heading = heading - 360
+                
             writer.writerow([name, longitude, latitude, altitude, heading, tilt,SPEED, 'AUTO','SHOOT'])
 
 def extract_waypoint_to_UGCS_csv(kml_file, output_csv):
@@ -58,6 +88,6 @@ def extract_waypoint_to_UGCS_csv(kml_file, output_csv):
 
 
 if __name__ == '__main__':
-    extract_waypoint_to_DJI_csv(kml_file, output_csv)
-    extract_waypoint_to_UGCS_csv(kml_file, output_csv_UGCS)
+    # extract_waypoint_to_DJI_csv(kml_file, output_csv)
+    extract_waypoint_info_to_UGCS_csv_v2(kml_file, output_csv_UGCS)
     print("Done")
